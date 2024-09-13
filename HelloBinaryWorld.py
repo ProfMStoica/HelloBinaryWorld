f887407722eee9da319fe6b567005aabbc62b5f7
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-13 16:21:18 -0400

    Use concatantion to make the greeting and fix output

--- HelloBinaryWorld.py
+++ HelloBinaryWorld.py
@@ -1,4 +1,4 @@
-"""Translate “Hello world” in binary and say hello to the user in binary language."""
+"""Translate "Hello world" in binary and say hello to the user in binary language."""
 
 #Translate "Hello world in binary". How?
 #Translate H into binary
@@ -16,8 +16,14 @@ lBinOutput = bin(ord('l'))[2:]
 oBinOutput = bin(ord('o'))[2:]
 
 #compose the greeting out of of its parts
-greeting = f"{hBinOutput} {eBinOutput}, {lBinOutput} {lBinOutput}, {oBinOutput}"
+greeting = f"{hBinOutput} {eBinOutput} {lBinOutput} {lBinOutput} {oBinOutput}"
 
 #print the translated greeting to the user
 print(greeting)
+
+#another way to compose the greeting is with concatation (adding multiple strings together)
+concatGreeting  = hBinOutput + ' ' + eBinOutput + ' ' + lBinOutput + ' ' + lBinOutput + ' ' + oBinOutput
+
+#print the concatenated greeting
+print(concatGreeting)
 
