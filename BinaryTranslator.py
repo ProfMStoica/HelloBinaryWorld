e2d9dfd53847fb22c86a3daee77fe7befd4cce15
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-31 14:30:14 -0400

    Ignore precompiled files

--- BinaryTranslator.py
+++ BinaryTranslator.py
@@ -2,14 +2,25 @@
 
 """Translate text into its binary representation"""
 def translateText(textToTranslate):
-    binTranslation = "TBD"
-    #TODO: implement text translation
-    pass
+    binTranslation = ""
+    
+    #repeat translation each character in the input text
+    for index in range(len(textToTranslate)):
+        #get the numeric value of the current character
+        numValue = ord(textToTranslate[index])
+        
+        #translate the numberic value into binary
+        binValue = bin(numValue)
+        
+        #add the translated character to the translation
+        binTranslation =  binTranslation + binValue[2:] + " "
     return binTranslation
 
 
 """Translate a given number into its binary representaiton"""
-def translateNum():
+def translateNum(numToTranslate):
     #TODO: implement number translation
+    binTranslation = ""
     pass
+    return binTranslation
 
